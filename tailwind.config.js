3170a2682cec10d79ade079b733c857fe9a626a6
phxbinh <phxbinh@gmail.com>
2026-02-05 03:16:55 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js (hoặc .ts)
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    // thêm nếu có folder khác
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neonCyan: '#22d3ee',
+        neonPurple: '#a855f7',
+      },
+      animation: {
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+} satisfies Config;
