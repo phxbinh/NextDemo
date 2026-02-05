936b6cf613a9899ac1ae71e4263986ee1559f95d
phxbinh <phxbinh@gmail.com>
2026-02-05 14:26:48 +0700

    Create About.tsx

---
+++ src/app/(public)/about/About.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main className="p-8">
+      <h1 className="text-4xl font-bold">Về Todo App Neon JS</h1>
+      <p className="mt-4">Ứng dụng todo với vibe neon cyberpunk...</p>
+    </main>
+  );
+}
