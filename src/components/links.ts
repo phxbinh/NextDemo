9ee209b3202557d775d693b097b46817d7eab9bf
phxbinh <phxbinh@gmail.com>
2026-02-05 17:00:48 +0700

    Create links.ts

---
+++ src/components/links.ts
@@ -0,0 +1,7 @@
+const sidebarLinks = [
+    { name: 'Dashboard', href: '/dashboard' },
+    { name: 'Todos', href: '/todos' },
+    { name: 'Profile', href: '/profile' },
+    { name: 'Settings', href: '/settings' },
+    { name: 'About', href: '/about' },
+  ];
