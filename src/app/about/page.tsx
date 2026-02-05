0f5d76f2cfddc6b0cdda829bdb45624d52c4784f
phxbinh <phxbinh@gmail.com>
2026-02-05 14:28:21 +0700

    Delete src/app/about directory

--- src/app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <main className="p-8">
-      <h1 className="text-4xl font-bold">Về Todo App Neon JS</h1>
-      <p className="mt-4">Ứng dụng todo với vibe neon cyberpunk...</p>
-    </main>
-  );
-}
