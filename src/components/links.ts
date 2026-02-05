f4acae85978cb589f6dc11473ecad6e52976da79
phxbinh <phxbinh@gmail.com>
2026-02-05 17:06:10 +0700

    Update links.ts

--- src/components/links.ts
+++ src/components/links.ts
@@ -1,7 +1,7 @@
 export const sidebarLinks = [
-    { name: 'Dashboard', href: '/dashboard' },
-    { name: 'Todos', href: '/todos' },
-    { name: 'Profile', href: '/profile' },
-    { name: 'Settings', href: '/settings' },
-    { name: 'About', href: '/about' },
+    { name: 'Dashboard', href: '/dashboard', showInSidebar: true },
+    { name: 'Todos', href: '/todos', showInSidebar: true },
+    { name: 'Profile', href: '/profile', showInSidebar: true },
+    { name: 'Settings', href: '/settings', showInSidebar: true },
+    { name: 'About', href: '/about', showInSidebar: true },
   ];
