61b3c38205761b2e1bed889894d160bee8b080a8
phxbinh <phxbinh@gmail.com>
2026-02-06 22:07:48 +0700

    Update auth.ts

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -50,7 +50,7 @@ import { supabaseServerAction } from '../../lib/supabase/server';
 import { redirect } from 'next/navigation';
 
 export async function signUp(formData: FormData) {
-  const supabase = supabaseServer(); // ✅ GỌI FUNCTION
+  const supabase = supabaseServerAction(); // ✅ GỌI FUNCTION
 
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
