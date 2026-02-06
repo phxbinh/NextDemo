5d0a25fb37f9185d3c79f3b8a3e6ac302dd0c90b
phxbinh <phxbinh@gmail.com>
2026-02-06 12:08:37 +0700

    Create page.tsx

---
+++ src/app/(app)/todos/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { getTodoById } from '../../../../server/db';
+import { updateTodoAction, deleteTodoFromDetail } from '../../../actions';
+import { notFound, redirect } from 'next/navigation';
+
+export default async function TodoDetail({ params }) {
+  const todo = await getTodoById(params.id);
+
+  if (!todo) notFound();
+
+  return (
+    <div className="max-w-xl mx-auto mt-20 space-y-6">
+      <h1 className="text-3xl font-bold text-white">Todo Detail</h1>
+
+      {/* Update */}
+      <form action={updateTodoAction} className="space-y-4">
+        <input type="hidden" name="id" value={todo.id} />
+        <input
+          name="title"
+          defaultValue={todo.title}
+          className="w-full px-4 py-3 rounded-xl bg-white/10 text-white outline-none"
+        />
+        <button
+          type="submit"
+          className="px-6 py-3 rounded-xl bg-cyan-500 hover:bg-cyan-400 transition"
+        >
+          Cập nhật
+        </button>
+      </form>
+
+      {/* Delete */}
+      <form
+        action={async (formData) => {
+          'use server';
+          await deleteTodoFromDetail(formData);
+          redirect('/todos');
+        }}
+      >
+        <input type="hidden" name="id" value={todo.id} />
+        <button
+          type="submit"
+          className="text-red-400 hover:text-red-300"
+        >
+          Xóa todo
+        </button>
+      </form>
+    </div>
+  );
+}
