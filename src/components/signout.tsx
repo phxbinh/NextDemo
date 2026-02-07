c86bd59d4c691a615e036e40a7ca5693f1cd2c52
phxbinh <phxbinh@gmail.com>
2026-02-07 21:44:13 +0700

    Update signout.tsx

--- src/components/signout.tsx
+++ src/components/signout.tsx
@@ -1,17 +1,3 @@
-
-/*
-'use client';
-
-import { signOut } from '../app/actions/auth';
-
-export function LogoutButton() {
-  return (
-    <form action={signOut}>
-      <button className="text-red-500">Logout</button>
-    </form>
-  );
-}
-*/
 'use client'
 import { signOut } from '../app/actions/auth';
 
