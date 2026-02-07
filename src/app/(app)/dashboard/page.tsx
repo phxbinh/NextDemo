b6b1b791e4ffe5fdca659e5468f84511d82b9b85
phxbinh <phxbinh@gmail.com>
2026-02-07 21:47:30 +0700

    Update page.tsx

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,33 +1,3 @@
-/*
-export default function DashboardPage() {
-  return (
-    <section className="p-6">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-      <p className="mt-2 text-gray-400">
-        Trang dashboard cơ bản, chưa có data.
-      </p>
-    </section>
-  );
-}*/
-
-/* chạy được với (chạy được) ở lib/supabase/server.ts
-import { supabaseServer } from '../../../lib/supabase/server';
-import { redirect } from 'next/navigation';
-
-export default async function DashboardPage() {
-  const supabase = supabaseServer();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    redirect('/login');
-  }
-
-  return <h1>Welcome {user.email}</h1>;
-}
-*/
 
 import { supabaseServerComponent } from '../../../lib/supabase/server';
 import { redirect } from 'next/navigation';
