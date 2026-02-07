4b4f54ebe34ef9d179040355d4dd5198a3eebede
phxbinh <phxbinh@gmail.com>
2026-02-07 17:55:27 +0700

    Update context.ts

--- src/lib/neon/context.ts
+++ src/lib/neon/context.ts
@@ -5,11 +5,26 @@ import { sql } from "./sql";
  * Set app.user_id cho RLS
  * PHẢI gọi mỗi request
  */
+/*
 export async function withUserContext<T>(
   userId: string,
   fn: () => Promise<T>
 ): Promise<T> {
   // set local cho transaction hiện tại
   await sql`SET app.user_id = ${userId}`;
+  return fn();
+}
+*/
+
+
+// lib/neon/context.ts
+//import { sql } from "./sql";
+
+export async function withUserContext<T>(
+  userId: string,
+  fn: () => Promise<T>
+): Promise<T> {
+  // ⚠️ SET KHÔNG ĐƯỢC DÙNG BIND PARAM
+  await sql.unsafe(`SET app.user_id = '${userId}'`);
   return fn();
 }
