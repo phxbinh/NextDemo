b4e76f268d2b3a5747011d05655264d42c56aa4b
phxbinh <phxbinh@gmail.com>
2026-02-08 14:39:49 +0700

    Update page.tsx

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -20,6 +20,12 @@ export default function Home() {
         <h1 className="text-3xl font-bold text-center mb-10">Các gói hosting</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4 md:px-8">
           <PricingCardCaNhanPlus />
+          <PricingCardCaNhanPlus />
+          <PricingCardCaNhanPlus />
+          <PricingCardCaNhanPlus />
+          <PricingCardCaNhanPlus />
+          <PricingCardCaNhanPlus />
+          <PricingCardCaNhanPlus />
           {/* Thêm các card khác nếu có */}
         </div>
       </>
