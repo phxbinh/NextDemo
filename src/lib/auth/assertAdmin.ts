6eaf07904171902b135f28354581cd24192b6156
phxbinh <phxbinh@gmail.com>
2026-02-08 13:13:31 +0700

    Update assertAdmin.ts

--- src/lib/auth/assertAdmin.ts
+++ src/lib/auth/assertAdmin.ts
@@ -2,8 +2,23 @@
 import { requireAdmin } from './requireAdmin';
 import { ForbiddenError } from '../errors';
 
+/*
 export async function assertAdmin(userId: string) {
   if (!(await requireAdmin(userId))) {
     throw new ForbiddenError();
   }
 }
+*/
+
+// lib/auth/assertAdmin.ts
+export async function assertAdmin(userId: string) {
+  try {
+    await requireAdmin(userId);
+  } catch {
+    throw new ForbiddenError();
+  }
+}
+
+
+
+
