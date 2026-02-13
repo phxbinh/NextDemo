b1236db1d46809982188f15def4e3eb77651196c
phxbinh <phxbinh@gmail.com>
2026-02-13 19:55:43 +0700

    Update page.tsx

--- src/app/(app)/admin/products/page.tsx
+++ src/app/(app)/admin/products/page.tsx
@@ -1,47 +1,4 @@
 
-
-
-
-
-/*
-import { headers } from 'next/headers';
-import { redirect } from 'next/navigation';
-
-type User = {
-  user_id: string;
-  avatar_url: string;
-  role: string;
-};
-
-export default async function AdminUsersPage() {
-  const h = await headers();
-
-  const host = h.get('host')!;
-  const protocol =
-    process.env.NODE_ENV === 'development' ? 'http' : 'https';
-
-  const res = await fetch(`${protocol}://${host}/api/admin/users`, {
-    cache: 'no-store',
-    headers: {
-      cookie: h.get('cookie') ?? '',
-    },
-  });
-
-  if (res.status === 401) redirect('/login');
-  if (res.status === 403) redirect('/403');
-  if (!res.ok) throw new Error('Failed to fetch users');
-
-  const users: User[] = await res.json();
-*/
-
-
-
-
-
-
-
-
-
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
@@ -58,19 +15,6 @@ interface Product {
 
 async function getProducts(): Promise<Product[]> {
 
-/*
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_APP_URL}/api/admin/products`,
-    {
-      cache: "no-store",
-    }
-  )
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch products")
-  }
-*/
-
   const h = await headers();
 
   const host = h.get('host')!;
@@ -83,6 +27,7 @@ async function getProducts(): Promise<Product[]> {
       cookie: h.get('cookie') ?? '',
     },
   });
+
   if (res.status === 401) redirect('/login');
   if (res.status === 403) redirect('/403');
   if (!res.ok) throw new Error('Failed to fetch users');
