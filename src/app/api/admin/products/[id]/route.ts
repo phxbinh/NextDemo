f7430c80b10efa732d06ba84870099c06b407501
phxbinh <phxbinh@gmail.com>
2026-02-13 13:05:26 +0700

    Create route.ts

---
+++ src/app/api/admin/products/[id]/route.ts
@@ -0,0 +1,73 @@
+import { NextResponse } from "next/server";
+import { sql } from '../../../../../lib/neon/sql';
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+
+    const result = await sql`
+      select *
+      from products
+      where id = ${params.id}
+      limit 1
+    `
+
+    if (result.length === 0) {
+      return NextResponse.json(
+        { error: "Product not found" },
+        { status: 404 }
+      )
+    }
+
+    return NextResponse.json(result[0])
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Forbidden" },
+      { status: 403 }
+    )
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+
+    const body = await req.json()
+
+    const {
+      name,
+      slug,
+      product_type,
+      short_description,
+      description,
+      status
+    } = body
+
+    const result = await sql`
+      update products
+      set
+        name = ${name},
+        slug = lower(${slug}),
+        product_type = ${product_type},
+        short_description = ${short_description},
+        description = ${description},
+        status = ${status}
+      where id = ${params.id}
+      returning *
+    `
+
+    return NextResponse.json(result[0])
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Update failed" },
+      { status: 400 }
+    )
+  }
+}
+
+
+
+
