50ff5be0ead886c1417fbde0fcb4acc021cf325c
phxbinh <phxbinh@gmail.com>
2026-02-15 22:05:04 +0700

    Update links.ts

--- src/components/links.ts
+++ src/components/links.ts
@@ -1,14 +1,16 @@
 export const sidebarLinks = [
     { name: 'Dashboard', href: '/dashboard', showInSidebar: true },
     { name: 'Todos', href: '/todos', showInSidebar: true },
-    { name: 'Profile', href: '/profile', showInSidebar: true },
-    { name: 'Settings', href: '/settings', showInSidebar: true },
+    //{ name: 'Profile', href: '/profile', showInSidebar: true },
+    //{ name: 'Settings', href: '/settings', showInSidebar: true },
     { name: 'About', href: '/about', showInSidebar: true },
     { name: 'Products', href: '/admin/products', showInSidebar: true },
   { name: 'Todo no images', href: '/todoimages', showInSidebar: true },
   { name: 'Todo with images', href: '/todowithimage', showInSidebar: true },
   { name: 'Add products', href: '/admin/products/new', showInSidebar: true },
      { name: 'Cre.Product types', href: '/admin/product-types', showInSidebar: true },
+    { name: 'Cre.Product types', href: '/admin/product-types/attribute-manager', showInSidebar: true },
+ 
      { name: 'Cre.attributes', href: '/admin/attributes', showInSidebar: true },
      { name: 'Products full', href: '/admin/products-full', showInSidebar: true },
   ];
