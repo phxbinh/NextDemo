a7421ac3d68eb3659464bea078f5ccb881dccba2
phxbinh <phxbinh@gmail.com>
2026-02-16 00:56:12 +0700

    Update links.ts

--- src/components/links.ts
+++ src/components/links.ts
@@ -13,4 +13,5 @@ export const sidebarLinks = [
  
      { name: 'Cre.attributes', href: '/admin/attributes', showInSidebar: true },
      { name: 'Products full', href: '/admin/products-full', showInSidebar: true },
+    { name: 'Products view', href: '/admin/products-view', showInSidebar: true },
   ];
