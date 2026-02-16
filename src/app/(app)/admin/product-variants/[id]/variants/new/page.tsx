fa93c35dd536d40b02a2657551058cfe4deec416
phxbinh <phxbinh@gmail.com>
2026-02-16 21:28:40 +0700

    Update page.tsx

--- src/app/(app)/admin/product-variants/[id]/variants/new/page.tsx
+++ src/app/(app)/admin/product-variants/[id]/variants/new/page.tsx
@@ -1,24 +1,4 @@
-/*
-import VariantForm from "@/components/product-variants/VariantForm";
-import VariantManager from "./CteateVariant";
-
-export default async function NewVariantPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Add Variant</h1>
-
-      <VariantManager productId={id} />
-    </div>
-  );
-}
-*/
-
+// src/app/(app)/admin/product-variants/[id]/variants/new/page.tsx
 import { sql } from "@/lib/neon/sql";
 import VariantManager from "./CteateVariant";
 
