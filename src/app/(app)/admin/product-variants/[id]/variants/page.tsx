74722c6b488139522cfbc712520bfb0ea0362182
phxbinh <phxbinh@gmail.com>
2026-02-16 14:20:14 +0700

    Update page.tsx

--- src/app/(app)/admin/product-variants/[id]/variants/page.tsx
+++ src/app/(app)/admin/product-variants/[id]/variants/page.tsx
@@ -1,23 +1,42 @@
+
+
+import React from "react";
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import Link from "next/link";
-import {DeleteButton} from "@/components/product-variants/DeleteButton";
+import DeleteButton from "@/components/product-variants/DeleteButton";
 
-async function getVariants(productId: string) {
-  const res = await fetch(
-    `/api/admin/product-variants?productId=${productId}`,
-    { cache: "no-store" }
-  );
+async function getVariants(id: string) {
+  const h = await headers();
 
-  if (!res.ok) throw new Error("Failed to fetch variants");
+  const host = h.get('host')!;
+  const protocol =
+    process.env.NODE_ENV === 'development' ? 'http' : 'https';
 
-  return res.json();
+  const res = await fetch(`${protocol}://${host}/api/admin/product-variants/${id}`, {
+    cache: 'no-store',
+    headers: {
+      cookie: h.get('cookie') ?? '',
+    },
+  });
+
+  if (res.status === 401) redirect('/login');
+  if (res.status === 403) redirect('/403');
+  if (!res.ok) throw new Error('Failed to fetch users');
+
+  return res.json()
+
 }
+
 
 export default async function VariantsPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const variants = await getVariants(params.id);
+  const { id } = await params;
+
+  const variants = await getVariants(id);
 
   return (
     <div className="p-6">
@@ -49,7 +68,7 @@ export default async function VariantsPage({
               <td className="p-2 border">{v.stock}</td>
               <td className="p-2 border space-x-2">
                 <Link
-                  href={`/admin/products/${params.id}/variants/${v.id}`}
+                  href={`/admin/product-variants/${id}/variants/${v.id}`}
                   className="text-blue-600"
                 >
                   Edit
