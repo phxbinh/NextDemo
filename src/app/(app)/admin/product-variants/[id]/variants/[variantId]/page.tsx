78b3afe843ec9ca7e19235fa7351347792ba30e7
phxbinh <phxbinh@gmail.com>
2026-02-16 22:37:35 +0700

    Update page.tsx

--- src/app/(app)/admin/product-variants/[id]/variants/[variantId]/page.tsx
+++ src/app/(app)/admin/product-variants/[id]/variants/[variantId]/page.tsx
@@ -1,19 +1,20 @@
 
-
-import VariantForm from "@/components/VariantForm";
+// src/app/(app)/admin/product-variants/[id]/variants/[variantId]/pqge.tsx
+import VariantForm from "@/components/product-variants/VariantForm";
 
 export default function EditVariantPage({
   params,
 }: {
-  params: { id: string; variantId: string };
+  params: Promise<{ id: string; variantId: string }>;
 }) {
+  const { id, variantId } = await params;
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Edit Variant</h1>
 
       <VariantForm
-        productId={params.id}
-        variantId={params.variantId}
+        productId={id}
+        variantId={variantId}
       />
     </div>
   );
