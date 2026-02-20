2dec08bc088fe34302589e5b59a6f4e184ce26ca
phxbinh <phxbinh@gmail.com>
2026-02-20 17:43:14 +0700

    Update ImageGallery.tsx

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -307,7 +307,7 @@ useEffect(() => {
 */}
 
 {images.length > 1 && (
-  <div className="overflow-hidden min-w-0">
+  <div className="w-[410px] overflow-x-hidden min-w-0">
     <div
       ref={thumbnailsRef}
       className="
