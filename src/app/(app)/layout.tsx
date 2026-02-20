e4acc6849fce5ff42787ebe75dfd3bbd93f032f6
phxbinh <phxbinh@gmail.com>
2026-02-20 15:26:16 +0700

    Update layout.tsx

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -53,7 +53,7 @@ return (
     </main>
 */}
 
-<main className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden px-2 py-6">
+<main className="flex-1 overflow-y-auto overflow-x-hidden px-2 py-6">
   {children}
 </main>
 
