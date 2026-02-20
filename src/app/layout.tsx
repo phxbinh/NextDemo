be6c99beadcd7eedacffe59f8d867a9c963edf92
phxbinh <phxbinh@gmail.com>
2026-02-20 17:27:01 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
 };
 
 // app/layout.tsx
+/*
 export default function RootLayout({
   children,
 }: {
@@ -22,4 +23,19 @@ export default function RootLayout({
     </html>
   );
 }
+*/
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="vi" className="h-full overflow-x-hidden">
+      <body className="min-h-screen overflow-x-hidden bg-gradient-to-br from-gray-950 via-purple-950 to-indigo-950 text-gray-100">
+        {children}
+      </body>
+    </html>
+  );
+}
+
 
