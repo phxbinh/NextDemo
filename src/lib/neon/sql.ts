bdb8137d14d94f68de6074cb0454516072650e4b
phxbinh <phxbinh@gmail.com>
2026-02-25 20:45:51 +0700

    Update sql.ts

--- src/lib/neon/sql.ts
+++ src/lib/neon/sql.ts
@@ -1,4 +1,7 @@
-// lib/neon/sql.ts
+// src/lib/neon/sql.ts
 import { neon } from "@neondatabase/serverless";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
 export const sql = neon(process.env.DATABASE_URL!);
