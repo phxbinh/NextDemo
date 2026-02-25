9d0199f174ed0f4d1840180fdef86a203a9d03b5
phxbinh <phxbinh@gmail.com>
2026-02-26 00:16:13 +0700

    Update profiles.ts

--- src/lib/neon/profiles.ts
+++ src/lib/neon/profiles.ts
@@ -13,17 +13,17 @@ export async function getAllProfiles(): Promise<Profile[]> {
   //const result = await sqlApp`SELECT current_user`
   //console.log(result) //-> app_user
 
-//const rowss = await sqlApp`SELECT * FROM rls_test`
-//console.log(rowss) //-> []
+const rowss = await sqlApp`SELECT * FROM rls_test`
+console.log(rowss) //-> []
 
-
+/*
 await sqlApp`BEGIN`
 await sqlApp`SET LOCAL app.user_id = '11111111-1111-1111-1111-111111111111'`
 const rowss = await sqlApp`SELECT * FROM rls_test`
 await sqlApp`COMMIT`
 
 console.log(rowss)
-
+*/
 
 
 
