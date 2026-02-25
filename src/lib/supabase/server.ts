a3ba8eaf4867ff445e14d1b1365d7c697f74136c
phxbinh <phxbinh@gmail.com>
2026-02-25 16:28:26 +0700

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,7 +5,10 @@ import {
   createServerComponentClient,
   createServerActionClient,
 } from '@supabase/auth-helpers-nextjs';
+import { createServerClient } from '@supabase/ssr'
 
+
+/*
 export function supabaseServerComponent() {
   return createServerComponentClient({ cookies });
 }
@@ -13,11 +16,55 @@ export function supabaseServerComponent() {
 export function supabaseServerAction() {
   return createServerActionClient({ cookies });
 }
+*/
 
 export const supabaseServer = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! // server only
 );
 
+
+// Thay thế cho hai functions được comment ở trên
+// lib/supabase/server.ts
+
+export function supabaseServerComponent() {
+  const cookieStore = cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (name) => cookieStore.get(name)?.value,
+        set: (name, value, options) => {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove: (name, options) => {
+          cookieStore.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
+}
+
+export function supabaseServerAction() {
+  const cookieStore = cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (name) => cookieStore.get(name)?.value,
+        set: (name, value, options) => {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove: (name, options) => {
+          cookieStore.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
+}
 
 
