a29b0971830128cc063ba8e230dad59b280a594e
phxbinh <phxbinh@gmail.com>
2026-02-26 22:23:37 +0700

    Update sql.ts

--- src/lib/neon/sql.ts
+++ src/lib/neon/sql.ts
@@ -4,7 +4,9 @@ import { neon } from "@neondatabase/serverless";
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
+// Sử dụng nếu muốn byPass toàn bộ RLS
 export const sql = neon(process.env.DATABASE_URL);
+export const sqlAdmin = neon(process.env.DATABASE_URL!)
 
+// Sử dụng để lấy data trong Neon PostgreSQL với RLS được bật
 export const sqlApp = neon(process.env.DATABASE_URL_APP!)
-//export const sqlAdmin = neon(process.env.DATABASE_URL!)
