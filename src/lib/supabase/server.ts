2967b18d4b6bbe1986d7bfa123e423a8e6b11cfd
phxbinh <phxbinh@gmail.com>
2026-02-28 13:33:10 +0700

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr'
 
+// Thận trọng khi dùng cái này. vì nó bypass mọi rls và policies ở DB
 export const supabaseServer = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! // server only
@@ -51,96 +52,6 @@ export async function createSupabaseServerClient() {
   )
 }
 
-
-// src/lib/supabase/server.ts -> Xoá
-/*
-export async function supabaseServerComponent() {
-  const cookieStore = await cookies()
-
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-        set(
-          name: string,
-          value: string,
-          options: {
-            path?: string
-            maxAge?: number
-            expires?: Date
-            httpOnly?: boolean
-            secure?: boolean
-            sameSite?: 'lax' | 'strict' | 'none'
-          }
-        ) {
-          cookieStore.set({ name, value, ...options })
-        },
-        remove(
-          name: string,
-          options: {
-            path?: string
-            maxAge?: number
-            expires?: Date
-            httpOnly?: boolean
-            secure?: boolean
-            sameSite?: 'lax' | 'strict' | 'none'
-          }
-        ) {
-          cookieStore.set({ name, value: '', ...options })
-        },
-      },
-    }
-  )
-}
-
-
-export async function supabaseServerAction() {
-  const cookieStore = await cookies()
-
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-        set(
-          name: string,
-          value: string,
-          options: {
-            path?: string
-            maxAge?: number
-            expires?: Date
-            httpOnly?: boolean
-            secure?: boolean
-            sameSite?: 'lax' | 'strict' | 'none'
-          }
-        ) {
-          cookieStore.set({ name, value, ...options })
-        },
-        remove(
-          name: string,
-          options: {
-            path?: string
-            maxAge?: number
-            expires?: Date
-            httpOnly?: boolean
-            secure?: boolean
-            sameSite?: 'lax' | 'strict' | 'none'
-          }
-        ) {
-          cookieStore.set({ name, value: '', ...options })
-        },
-      },
-    }
-  )
-}
-*/
 
 
 
