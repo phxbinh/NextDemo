430b556ab22b5353f196983f081b9521352402d8
phxbinh <phxbinh@gmail.com>
2026-03-01 11:31:14 +0700

    Update TodoImage.tsx

--- src/components/TodoImage.tsx
+++ src/components/TodoImage.tsx
@@ -1,20 +1,5 @@
 import { getPublicImageUrl } from '../lib/supabase/publicUrl';
 
-/*
-export function TodoImage({ path }: { path: string }) {
-  const url = getPublicImageUrl(path);
-
-  return (
-    <img
-      src={url}
-      alt=""
-      className="rounded-lg object-cover w-full h-40"
-      loading="lazy"
-    />
-  );
-}
-*/
-
 
 export function TodoImage({
   path,
