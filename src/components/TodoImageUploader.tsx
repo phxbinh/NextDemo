13e614627e091c8bd04039b1f4b66f981aa74c1f
phxbinh <phxbinh@gmail.com>
2026-03-12 17:08:03 +0700

    Update TodoImageUploader.tsx

--- src/components/TodoImageUploader.tsx
+++ src/components/TodoImageUploader.tsx
@@ -106,7 +106,7 @@ export default function TodoImageUploader({ todoId }: { todoId: string }) {
       const path = `${todoId}/${crypto.randomUUID()}-${file.name}`;
 
       const { error } = await supabase.storage
-        .from('todo-images')
+        .from('products-images')
         .upload(path, file, { upsert: false });
 
       if (error) {
